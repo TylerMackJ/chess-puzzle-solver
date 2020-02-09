bda1f8e6c1820f8ba0a8d32cafdcc13c54151953
aj132608 <32584157+aj132608@users.noreply.github.com>
2020-02-09 15:25:48 -0600

    Added a function to populate the chess board.

---
+++ src/groupName/cps/game/PopulateBoard.java
@@ -0,0 +1,72 @@
+package groupName.cps.game;
+import groupName.cps.game.*;
+import groupName.cps.game.Piece;
+
+public class PopulateBoard {
+    public Board board;
+    public String pieceLocations;
+
+
+    public PopulateBoard(Board board, String pieceLocations) {
+        this.board = board;
+        this.pieceLocations = pieceLocations;
+    }
+
+    public Board getPopulatedBoard() {
+        // Separate the pieceLocations String into a String array
+        String[] locationArray = this.pieceLocations.split(" ");
+        Piece.Color currentColor = null;
+        Piece.Type currentType = null;
+        int currentX = 0;
+        int currentY = 0;
+
+        for (int lcv=0; lcv<locationArray.length; lcv++) {
+            // Separate the element into a character array
+            char[] currentCharacters = locationArray[lcv].toCharArray();
+
+            // Determine what the color is
+            switch (currentCharacters[0]){
+                case 'W': currentColor = Piece.Color.WHITE;
+                break;
+                case 'B': currentColor = Piece.Color.BLACK;
+                break;
+            }
+
+            // Determine what the piece type is
+            switch (currentCharacters[1]){
+                case 'K': currentType = Piece.Type.KING;
+                break;
+                case 'Q': currentType = Piece.Type.QUEEN;
+                break;
+                case 'k': currentType = Piece.Type.KNIGHT;
+                break;
+                case 'B': currentType = Piece.Type.BISHOP;
+                break;
+                case 'R': currentType = Piece.Type.ROOK;
+                break;
+                case 'P': currentType = Piece.Type.PAWN;
+                break;
+            }
+
+            // Create the Piece object
+            Piece currentPiece = new Piece(currentType, currentColor);
+
+            // Get the X value
+            //int a=Integer.parseInt(String.valueOf(c));
+            currentX =  Integer.parseInt(String.valueOf(currentCharacters[2]));
+
+            // Get the Y value
+            currentY =  Integer.parseInt(String.valueOf(currentCharacters[3]));
+
+            // Place the piece in the desired location on the board
+            this.board.board[currentX][currentY] = currentPiece;
+        }
+
+        return this.board;
+    }
+
+    public static void bullShit() {
+        Piece myPiece = new Piece(Piece.Type.KING, Piece.Color.WHITE);
+    }
+}
+
