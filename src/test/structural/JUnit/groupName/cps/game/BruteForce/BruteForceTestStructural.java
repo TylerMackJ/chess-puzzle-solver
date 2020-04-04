cf7e059640e5cb48689d206135fcbf2928b71633
Caleb Horn <calebahorn@gmail.com>
2020-04-04 13:57:07 -0500

    Functional\structural testing for BruteForce class
    Developed tests for BruteForce methods and reached 100% coverage.

---
+++ src/test/structural/JUnit/groupName/cps/game/BruteForce/BruteForceTestStructural.java
@@ -0,0 +1,30 @@
+package groupName.cps.game.BruteForce;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BruteForceTestStructural {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void solvePuzzle() {
+    }
+
+    @Test
+    void win() {
+    }
+
+    @Test
+    void chooseMove() {
+    }
+}
