c9a76614e3cfdbb64be78783215e3224715018e8
Haroon <haroonsaeed20@gmail.com>
2020-04-05 17:34:56 -0500

    Added new tests

---
+++ src/test/structural/JUnit/groupName/cps/game/StateTest.java
@@ -0,0 +1,36 @@
+package groupName.cps.game;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class StateTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    State s = new State();
+
+
+    @Test
+    void getState1() {
+        System.out.println("StateTest getState1()");
+        State s1 = new State();
+        assertNotEquals(true,s1);
+    }
+
+    @Test
+    void getState2() {
+        System.out.println("StateTest getState2()");
+        State s2 = new State(false, Piece.Color.WHITE);
+        assertNotEquals(true,s2);
+    }
+}
