4ef8af2befb2ac8ff7cb4b45f5f95cf9cbb105d1
Caleb Horn <calebahorn@gmail.com>
2020-04-05 17:12:13 -0500

    Added functional and structural tests for move class
    Added functional and structural tests for move class, ensuring to reach 100% coverage.

---
+++ src/test/functional/JUnit/groupName/cps/game/Movement/MoveTest.java
@@ -0,0 +1,53 @@
+package groupName.cps.game.Movement;
+
+import groupName.cps.game.Game;
+import groupName.cps.game.GenerateGame.GenerateGame;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MoveTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testToString() {
+        Move m = new Move(1,2,3,4);
+        assertEquals("(1, 2)->(3, 4)",m.toString());
+    }
+
+    @Test
+    void testEqualsTrue() {
+        Move m = new Move(1,2,3,4);
+        Move g = new Move(1,2,3,4);
+        assertEquals(true,m.equals(g));
+    }
+
+    @Test
+    void testEqualsFalse() {
+        Move m = new Move(1,2,3,4);
+        Move g = new Move(1,2,3,5);
+        assertEquals(false,m.equals(0));
+    }
+
+
+    @Test
+    void testHashCode() {
+        Move m = new Move(1,2,3,4);
+        assertEquals(31872,m.hashCode());
+    }
+}
