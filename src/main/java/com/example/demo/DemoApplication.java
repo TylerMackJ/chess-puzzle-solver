a3d656440181e50c775f4ffde901b43ceaffce03
Tyler Johnson <tylermackj@gmail.com>
2020-04-26 17:40:44 -0500

    Added the delete route on the win_game service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,14 @@ package com.example.demo;
 import groupName.cps.game.BruteForce.BruteForce;
 import groupName.cps.game.Game;
 import groupName.cps.game.Movement.Movement;
+import groupName.cps.game.Rest.PieceLocation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.PostRequest;
+import javax.validation.Valid;
 import groupName.cps.game.PopulateBoard;
 import groupName.cps.game.Piece;
 import groupName.cps.game.Movement.Move;
@@ -47,10 +49,10 @@ public class DemoApplication {
 //	}
 
 	//GET moves to win
-	@GetMapping("/get_status")
+	@GetMapping("/win_game")
 	public String getStatus() {
-		if(this.board == null) {
-			return "Waiting for a board configuration... Send one to http://localhost:8080/init_board";
+		if(this.gameInstance == null) {
+			return "Waiting for a board configuration... Send one to http://localhost:8080/win_game/init_board";
 		}
 		if(this.gameInstance == null) {
 			this.gameInstance = new Game(this.board);
@@ -70,7 +72,7 @@ public class DemoApplication {
 	}
 
 	//POST new board
-	@PostMapping(path="/init_board", consumes = "application/json", produces = "application/json")
+	@PostMapping(path="/win_game/init_board", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<PopulateBoard> setBoard(@RequestBody() PopulateBoard newBoard) {
 
 		this.board = newBoard.getPopulatedBoard();
@@ -78,5 +80,23 @@ public class DemoApplication {
 
 		return new ResponseEntity<PopulateBoard>(newBoard, HttpStatus.OK);
 	}
+
+	/*
+	@PutMapping(path="/win_game/add_piece", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Piece[][]> putBoard(@Valid @ResponseBody PieceLocation pieceLocation) {
+		if(this.gameInstance != null) {
+			this.gameInstance.board[pieceLocation.getLocation()[0]][pieceLocation.getLocation()[1]] = pieceLocation.getPiece();
+			return new ResponseEntity<Piece[][]>(this.gameInstance.board, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Piece[][]>(new Piece[0][0], HttpStatus.FAILED_DEPENDENCY);
+		}
+	}
+	 */
+
+	@DeleteMapping("/win_game/delete_board")
+	public String deleteBoard() {
+		this.gameInstance = null;
+
+		return "The board was deleted!";
+	}
 }
-            
