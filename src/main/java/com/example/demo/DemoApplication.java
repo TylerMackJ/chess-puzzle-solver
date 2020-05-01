6b537785255d949ea4c7ddcc2df462f04ff7448e
jlorenz37 <60797955+jlorenz37@users.noreply.github.com>
2020-05-01 15:45:50 -0500

    added delete

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -134,5 +134,14 @@ public class DemoApplication {
 		}
 	}
 
+	@DeleteMapping("/generate_game/delete_moves")
+	public String deleteMoves() {
+		if(moveCount != -1){
+			this.moveCount = -1;
+			return "Number of moves reset to -1";}
+		else
+			return "You need to post first!";
+	}
+
 
 }
